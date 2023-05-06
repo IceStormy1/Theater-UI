b32719b7428edfb90ef544d142b606041602b9f9
Tolmachev Mikhail <55889141+IceStormy1@users.noreply.github.com>
2023-05-06 18:31:05 +0300

    Add adding JWT token to header

--- src/axios.js
+++ src/axios.js
@@ -1,3 +1,16 @@
 import axios  from "axios";
 
 axios.defaults.baseURL = 'http://localhost:5000/api/';
+axios.interceptors.request.use(
+    config => {
+        const user = localStorage.getItem('user');
+
+        if (user) {
+            let accessToken = JSON.parse(user).accessToken;
+
+            config.headers.Authorization = `Bearer ${accessToken}`;
+        }
+        return config;
+    },
+    error => Promise.reject(error)
+);
--- src/services/auth/auth.header.js
+++
@@ -1,9 +0,0 @@
-export default function authHeader() {
-    let user = localStorage.getItem('user');
-
-    if (user != null) {
-        return { Authorization: 'Bearer ' + user.accessToken };
-    }
-
-    return {};
-}
